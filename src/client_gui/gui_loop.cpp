9f099f58aaca35776fd983c324dd12bb80b5d1c2
Mariano Lazzarini <marianolazzarini01@gmail.com>
2024-05-23 20:42:15 -0300

    agregado funciones llamadas para actualizar estado actual, y para llamar al renderer Se cambia el dropeado de ticks, para que pueda elegir no renderizar nada nuevo cuando el update del estado tomó mas del esperado se agregan prints de aviso para cuando se dropea un tick, y para cuando se cancela el renderizado

--- src/client_gui/gui_loop.cpp
+++ src/client_gui/gui_loop.cpp
@@ -5,7 +5,7 @@
 
 double get_random() {
     static int x = 0;
-    double random[] = {1.1, 1.2, 1.7, 5, 2.6};
+    double random[] = {0.90, 1.0, 1.7, 5, 2.6};
     x = (x + 1) % (sizeof(random) / sizeof(double));
 
     return random[x];
@@ -29,28 +29,38 @@ void GuiLoop::ejecutar() {
 
         */
 
-        // Procesado de cosas
-        {
-            auto duracion_render = (std::chrono::nanoseconds{
-                (int)((SECOND_IN_NANO / FPS) * get_random())});
+        // // Procesado de cosas
+        // {
+        //     auto duracion_render = (std::chrono::nanoseconds{
+        //         (int)((SECOND_IN_NANO / FPS) * get_random())});
 
-            std::this_thread::sleep_for(duracion_render);
-            std::cout << "TICK " << tick_actual << std::endl;
-            std::cout << "DURACION:" << std::endl;
-            std::cout << "\tRender: "
-                      << duration_cast<milliseconds>(duracion_render).count()
-                      << "ms" << std::endl;
-        }
+        //     std::this_thread::sleep_for(duracion_render);
+        // }
 
         time_point final_tick = inicio_tick + TICK_DURATION;
+
+        actualizar_estado();
+
         time_point tiempo_actual = reloj.now();
-
-        if (tiempo_actual < inicio_tick) {
-            std::cout << "ERROR" << std::endl;
-        }
-
         bool sobra_tiempo_en_tick = tiempo_actual <= final_tick;
 
+        if (sobra_tiempo_en_tick) {
+            // Ejecuto el renderer, solamente si todavía me sobra tiempo en el
+            // tick actual.
+            // En el caso de que ya haya consumido todo el tiempo del tick
+            // actual, Decido ni siquiera ejecutar el renderer para , tal vez,
+            // llegar al proximo tick a tiempo.
+            ejecutar_renderer();
+        } else {
+            std::cout << "Render cancelled. ";
+        }
+
+        tiempo_actual = reloj.now();
+        sobra_tiempo_en_tick = tiempo_actual <= final_tick;
+
+        // Tengo otro if, porque podría haber sobrado tiempo para renderizar,
+        // pero haberme pasado del tiempo mientras renderizaba.
+        // Por lo que tengo que checkear otra vez
         if (!sobra_tiempo_en_tick) {
             // Calculo la cantidad de ticks completos que usé
             uint ticks_extras_transcurridos =
@@ -58,6 +68,8 @@ void GuiLoop::ejecutar() {
 
             // Agrego cantidad de ticks dropeados
             tick_actual += ticks_extras_transcurridos;
+
+            std::cout << ticks_extras_transcurridos << " dropped ticks\n";
 
             // Agregado cantidad de tiempo salteado ("dropeado")
             inicio_tick += (TICK_DURATION * ticks_extras_transcurridos);
@@ -68,4 +80,23 @@ void GuiLoop::ejecutar() {
         tick_actual += 1;
         std::this_thread::sleep_until(final_tick);
     }
+}
+
+void GuiLoop::ejecutar_renderer() {
+    // TODO: acá matias debería poner el llamado al renderr
+    // renderer.renderizar(tick_actual, estadoActual);
+
+    std::cout << "tick: " << tick_actual << "\n";
+    return;
+}
+
+void GuiLoop::actualizar_estado() {
+    // TODO: Acá deberia codear la manera de actualizar el estado actual
+    // while (true) {
+    //     auto update = queueUpdates.tryrecv();
+    //     if (update == None)
+    //         break:
+
+    //     estadoActual.actualizar(update);
+    // }
 }
--- src/client_gui/gui_loop.h
+++ src/client_gui/gui_loop.h
@@ -13,12 +13,25 @@
 class GuiLoop {
     std::chrono::steady_clock reloj;
 
+    // TODO: Acá matias debería poner el renderer para que se cree
+    // Renderer renderer;
+
+    // TODO: estado que Mariano debe actualizar,
+    // TODO: a partir de las updates que recibe de un Queue?
+    // EstadoJuego estadoActual;
+    // Queue<update> queueUpdates;
+
     uint32_t tick_actual;
 
    public:
     GuiLoop();
 
     void ejecutar();
+
+   private:
+    void ejecutar_renderer();
+
+    void actualizar_estado();
 };
 
 #endif
