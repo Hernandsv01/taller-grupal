2903467bb5027f3aceb60d72927798eec6afc223
Laraconverso <laraconverso@gmail.com>
2024-05-25 16:23:21 -0300

    Se realizaron modificaciones en el enum de acciones

--- src/common/dtos.h
+++ src/common/dtos.h
@@ -2,11 +2,13 @@
 #define COMMON_DTOS_H
 
 enum ActionType{
+    NULL_ACTION = 0x00,
     JUMP = 0x01,
     SHOOT = 0x02,
     RUN_LEFT = 0x03,
     RUN_RIGHT = 0x04,
-    SPECIAL = 0x05 //esta accion es diferente para cada personaje (pero se evalua en el server).
+    SPECIAL = 0x05, //esta accion es diferente para cada personaje (pero se evalua en el server).
+    STOP_GAME = 0x10
 };
 
 struct ActionMessage{
