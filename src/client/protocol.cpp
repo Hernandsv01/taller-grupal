ad19076c4975db925b2302655b1a6216a38ae2b0
Laraconverso <laraconverso@gmail.com>
2024-05-27 22:24:35 -0300

    Se agrega metodo para recibir updates del juego.

--- src/client/protocol.cpp
+++ src/client/protocol.cpp
@@ -1,4 +1,6 @@
 #include "protocol.h"
+#include <sstream>
+#include <vector>
 
 ClientProtocol::ClientProtocol(Socket& socket) : socket(socket){}
 
@@ -6,11 +8,18 @@ ClientProtocol::ClientProtocol(Socket& socket) : socket(socket){}
 void ClientProtocol::send_action(ActionType& key_event) {
     bool was_closed = false;
     this->socket.sendall(&key_event, sizeof(uint8_t), &was_closed);
-    //evaluo if was closed y suelto excep.
+    //
 }
 
 //recive el paquete del server
-void ClientProtocol::receive_ticks() {
-
+std::vector<Update> ClientProtocol::receive_ticks() {
+    bool was_closed = false;
+    uint8_t quantity = 0x00;
+    std::vector<Update> update;
+    this->socket.recvall(&quantity, sizeof(uint8_t), &was_closed);
+    //
+    this->socket.recvall(&update.data(), sizeof(Update)*quantity, &was_closed); //falta el size of what que no me acuerdo como era
+    //
+    return update;
 }
 
