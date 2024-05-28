a4d4fb25092b03377b872fb02d5addfb3c26563c
Laraconverso <laraconverso@gmail.com>
2024-05-27 22:30:51 -0300

    se crea la queue compartida entre el updater y el renderer.

---
+++ src/client/update_queue.cpp
@@ -0,0 +1,18 @@
+#include "update_queue.h"
+
+void Update_queue::push(std::vector<Update> &update) {
+    updates.push(update);
+}
+
+void Update_queue::try_push(std::vector<Update> &update) {
+    updates.try_push(update);
+}
+
+std::vector<Update> Update_queue::pop() {
+    return updates.pop();
+}
+
+std::vector<Update> Update_queue::try_pop() {
+    return updates.try_pop();
+}
+
