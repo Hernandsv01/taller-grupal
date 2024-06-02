0042c246d739d9517e670616db62f1b03f82040a
Laraconverso <laraconverso@gmail.com>
2024-06-02 00:51:33 -0300

    Agrego estructura del archivo main comentado

--- src/client/client_main.cpp
+++ src/client/client_main.cpp
@@ -2,4 +2,15 @@
 
 int main(int argc, char* argv[]) {
     std::cout << "Hello client!" << std::endl;
+    // Estructura del main
+    /*
+     * create_lobby
+     * lobby_get_socket
+     * create_client
+     *      - protocolo inicializar partida
+     *      - eventlistener.start
+     *      - gui_loop
+     *          - renderer
+     *          - updater
+     */
 }
