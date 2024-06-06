1d1ee13ce50d93d3f1a236f5cfec14222c7f14bb
Mariano Lazzarini <marianolazzarini01@gmail.com>
2024-06-05 23:59:46 -0300

    Modificado del pullevent por waitevent Se elimino el while sino el programa nunca terminaba

--- src/client/eventlistener.cpp
+++ src/client/eventlistener.cpp
@@ -7,23 +7,24 @@ void EventListener::run() {
     SDL_Event event;
     bool running = true;
     while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            } else if ((event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) &&
-                       event.key.repeat == 0) {
-                // DEBUG:
-                // const char* eventType = (event.type == SDL_KEYDOWN)
-                //                             ? "Key pressed: "
-                //                             : "Key released: ";
-                // std::cout << eventType <<
-                // SDL_GetKeyName(event.key.keysym.sym)
-                //           << std::endl;
+        bool success = SDL_WaitEvent(&event);
 
-                ActionType action = this->mapper.map_key_event(event);
+        if (!success || event.type == SDL_QUIT) {
+            std::cout << "Quitting..." << std::endl;
+            running = false;
+        } else if ((event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) &&
+                   event.key.repeat == 0) {
+            // DEBUG:
+            // const char* eventType = (event.type == SDL_KEYDOWN)
+            //                             ? "Key pressed: "
+            //                             : "Key released: ";
+            // std::cout << eventType <<
+            // SDL_GetKeyName(event.key.keysym.sym)
+            //           << std::endl;
 
-                protocol.send_action(action);
-            }
+            ActionType action = this->mapper.map_key_event(event);
+
+            protocol.send_action(action);
         }
     }
 }
