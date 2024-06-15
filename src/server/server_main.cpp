f57c3b4573920628941a0c67c0201f1e14cff4e1
Laraconverso <laraconverso@gmail.com>
2024-06-15 00:50:21 -0300

    Restructuracion del server_main, utiliza GameManager

--- src/server/server_main.cpp
+++ src/server/server_main.cpp
@@ -2,6 +2,7 @@
 
 #include "Game.h"
 #include "clients/client_listener.h"
+#include "game/game_manager.h"
 
 int main(int argc, char* argv[]) {
     // Puerto hardcodeado para mas comodidad de ejecución y pruebas
@@ -11,6 +12,7 @@ int main(int argc, char* argv[]) {
     // }
     // Client_listener listener(argv[1]);
     char* puerto = "15500";
+    /*
     Client_listener listener(puerto);
     listener.start();
 
@@ -38,4 +40,32 @@ int main(int argc, char* argv[]) {
 
     game.stop();
     game.join();
+    */
+
+    //version con game manager
+    //no deberia tener el puerto hardcodeado!
+    Socket socket(puerto);
+    GameManager game_manager(socket);
+    game_manager.start();
+
+    //REVISAR MANEJO DE SOCKET EN EL LISTENER
+    Client_listener listener(socket);
+    listener.start();
+
+    while (Client_Monitor::getAll().empty()) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+
+    std::string input;
+    do {
+        std::cin >> input;
+    } while (input != "q");
+
+    listener.kill();
+    listener.stop();
+    listener.join();
+    game_manager.kill();
+    game_manager.stop();
+    game_manager.join();
+
 }
