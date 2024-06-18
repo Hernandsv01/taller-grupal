3c7463f9f8312e2e4ef2df31cc62d709befd1348
laraconverso <laraconverso@gmail.com>
2024-06-18 05:51:27 +0000

    Agrego comentarios de funcionalidad e ideas de implementacion

--- src/server/loader/config_loader.h
+++ src/server/loader/config_loader.h
@@ -3,21 +3,28 @@
 
 #include "yaml-cpp/yaml.h"
 
+//Deberia revisar las entidades y ver que otras cuestiones se puede configurar 
 
 
-struct Config{
-    GameConfig game;
-    EnemyConfig enemy;
-    PlayerConfig player;
-    AssetsConfig assets;
-}
+// struct Config{
+//     GameConfig game;
+//     EnemyConfig enemy;
+//     PlayerConfig player;
+//     AssetsConfig assets;
+// }
 
 class ConfigLoader{
 private:
 
 public: 
+    //cargar desde un archivo predeterminado algo, como respuestas me deberia generar 
+    //un dato de configuracion que permitirian que se configure en la creacion del Game.
+    //Capaz el constructor del juego debe recibir esta configucarion y cuando se crea 
+    //la partida se carga con ella.
     static int load(std::string file_name);
-    
+
+    // lectura del yaml especificamente... 
+
 };
 
 #endif // CONFIG_LOADER_H
