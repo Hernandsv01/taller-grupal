f58dde457ecf4bf1c2c867b24ab609f7bc0236f1
Lara Converso <63512648+Laraconverso@users.noreply.github.com>
2024-06-25 04:18:44 -0300

    Quito comentarios del autogenerado

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mi
 sudo apt-get install -y qtbase5-dev qt5-qmake
 sudo apt-get install -y libyaml-cpp-dev
 
+echo "Clono repositorio ..."
 #Clono el respositorio
 git clone https://github.com/Hernandsv01/taller-grupal.git /home/vagrant/taller-grupal
 
@@ -21,9 +22,4 @@ cd /home/vagrant/taller-grupal
 cmake .
 make
 
-#una terminal 
-#gnome-terminal -- bash -c "./server; exec bash"
-
-#otra therminal 
-#gnome-terminal -- bash -c "./client; exec bash"
 
